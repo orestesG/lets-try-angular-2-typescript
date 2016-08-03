c972ad741618948ee39417408a42f21a616ae275
Orestes <orestes.perez@gases.co.cu>
2016-08-03 14:00:57 -0400

    deleting from repo compiled files, since are being compiled when  run the compiler at `npm run start`

--- app/what-we-do/shared/category.model.js
+++
@@ -1,7 +0,0 @@
-var Category = (function () {
-    function Category() {
-    }
-    return Category;
-})();
-exports.Category = Category;
-//# sourceMappingURL=category.model.js.map
--- app/what-we-do/shared/mock-categories.js
+++
@@ -1,7 +0,0 @@
-exports.CATEGORIES = [
-    { id: 11, name: 'Agency Life' },
-    { id: 12, name: 'Client Work' },
-    { id: 13, name: 'Press' },
-    { id: 14, name: 'Thinking' }
-];
-//# sourceMappingURL=mock-categories.js.map
