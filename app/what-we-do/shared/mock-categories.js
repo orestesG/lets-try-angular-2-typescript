f4b2bcd41cd39c1310271fd23f28a68a298ed088
Orestes <orestesgabrielp@gmail.com>
2016-08-03 16:33:31 -0400

    deleting from repo compiled files, since are being compiled when  run the compiler at `npm run start`

--- app/what-we-do/shared/mock-categories.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.CATEGORIES = [
-    { id: 11, name: 'Agency Life' },
-    { id: 12, name: 'Client Work' },
-    { id: 13, name: 'Press' },
-    { id: 14, name: 'Thinking' }
-];
-//# sourceMappingURL=mock-categories.js.map
